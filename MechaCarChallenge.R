fbc366e61afa7bdf0193cb8917e1e0b63740d907
Rachel McKinnon <rachel.e.mckinnon@gmail.com>
2021-11-30 19:59:40 -0600

    Add deliverable 3 R-Script.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,3 +18,17 @@ total_summary <- Suspension_Coil_df%>%summarize("Mean" = mean(PSI),
 # lot summary data frame
 lot_summary <- Suspension_Coil_df%>%group_by(Manufacturing_Lot)%>%summarize("Mean" = mean(PSI), 
                                                 "Median"=median(PSI), "Variance"=var(PSI), "SD"=sd(PSI))
+#All lots t.test
+t.test(Suspension_Coil_df$PSI,mu=1500)
+
+#Lot 1 t.test
+lot1 <- subset(Suspension_Coil_df, Manufacturing_Lot == "Lot1", select = "PSI")
+t.test(lot1,mu=1500)
+
+#Lot 2 t.test
+lot2 <- subset(Suspension_Coil_df, Manufacturing_Lot == "Lot2", select = "PSI")
+t.test(lot2,mu=1500)
+
+#Lot 3 t.test
+lot3 <- subset(Suspension_Coil_df, Manufacturing_Lot == "Lot3", select = "PSI")
+t.test(lot3,mu=1500)
